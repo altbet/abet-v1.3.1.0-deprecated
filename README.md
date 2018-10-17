50ae070a5c51df0c7df2ac8198664ece8ac150c7
altbet <office@altbet.io>
2018-10-17 21:25:08 +0200

    🎲🎲🎲 Commit

---
+++ README.md
@@ -0,0 +1,185 @@
+![altbet](https://github.com/altbet/abet/raw/master/src/qt/res/images/about.png)
+
+<h2>What is Altbet?</h2>
+<p>Mutual betting is widely adopted in gambling and applied to horse racing, greyhound racing and sports betting. Altbet is the first platform that combines this model with blockchain technology, what allows to benefit from the advantages of own cryptocurrency.</p>
+<p>In the long-term perspective, Altbet aspires to become world’s leading online mutual betting platform.</p>
+<p>We do believe that having the best product / best offer is the key. Our team is planning to offer perfect platform for mutual betting and gambling which will be fully transparent thanks to our custom provably fair system. Altbet platform is and will be one of the kind.</p>
+<a href="https://altbet.io" title="altbet website" target="_blank">View our website for more details</a>
+<table>
+<tbody>
+<tr><td><strong>01.</strong></td><td>E-SPORT BETTING</td></tr>
+<tr><td><strong>02.</strong></td><td>SPORT BETTING</td></tr>
+<tr><td><strong>03.</strong></td><td>MARKET BETTING</td></tr>
+<tr><td><strong>04.</strong></td><td>SPECIAL EVENTS BETTING</td></tr>
+<tr><td><strong>05.</strong></td><td>CUSTOM GAMBLING GAMES</td></tr>
+<tr><td><strong>06.</strong></td><td>0% FEE ON OUR GAMES</td></tr>
+<tr><td><strong>07.</strong></td><td>CUSTOM PROVABLY FAIR SYSTEM</td></tr>
+<tr><td><strong>08.</strong></td><td>FULL BETTING ANONYMITY</td></tr>
+<tr><td><strong>09.</strong></td><td>HIGH END DEVELOPMENT</td></tr>
+<tr><td><strong>10.</strong></td><td>OWN CRYPTOCURRENCY</td></tr>
+</tbody>
+</table>
+<h3>Coin Specification</h3>
+<table>
+<tbody>
+<tr><td><strong>NAME</strong></td><td>Altbet</td></tr>
+<tr><td><strong>TICKER</strong></td><td>ABET</td></tr>
+<tr><td><strong>ALGORITHM</strong></td><td>Quark</td></tr>
+<tr><td><strong>TYPE</strong></td><td>POS / MN</td></tr>
+<tr><td><strong>TOTAL SUPPLY</strong></td><td>21 000 000 ABET</td></tr>
+<tr><td><strong>PREMINE</strong></td><td>210 000 ABET (1%)</td></tr>
+<tr><td><strong>COLLATERAL</strong></td><td>1 000 ABET</td></tr>
+<tr><td><strong>BLOCKS PER DAY</strong></td><td>1 440</td></tr>
+<tr><td><strong>BLOCK TIME</strong></td><td>60 SECONDS</td></tr>
+<tr><td><strong>STAKE MIN AGE</strong></td><td>1 HOUR</td></tr>
+<tr><td><strong>REWARDS</strong></td><td>80% MASTERNODE<br>20% STAKING</td></tr>
+<tr><td><strong>PORTS</strong></td><td>2238 P2P Port<br>2239 RPC Port</td></tr>
+</tbody>
+</table>
+<a href="https://altbet.io" title="altbet website" target="_blank">View our website for more details</a>
+<h3>Block Rewards</h3>
+<table>
+<tbody>
+<tr><th>PHASE</th><th>BLOCK</th><th>REWARD PER BLOCK</th><th>MASTERNODE REWARD</th><th>STAKING REWARD</th><th>ROUND TIME FRAME</th></tr>
+<tr>
+<td>PREMINE</td>
+<td>0 - 0</td>
+<td>210 000.00 ABET</td>
+<td>0.00 ABET</td>
+<td>0.00 ABET</td>
+<td>1 MINUTE</td>
+</tr><tr>
+<td>NETWORK INIT</td>
+<td>1 - 1000</td>
+<td>0.10 ABET</td>
+<td>0.08 ABET</td>
+<td>0.02 ABET</td>
+<td>17 HOURS</td>
+</tr><tr>
+<td>FAIR START</td>
+<td>1001 - 21160</td>
+<td>0.70 ABET</td>
+<td>0.56 ABET</td>
+<td>0.14 ABET</td>
+<td>14 DAYS</td>
+</tr><tr>
+<td>PHASE 1</td>
+<td>21161 - 31240</td>
+<td>2.00 ABET</td>
+<td>1.60 ABET</td>
+<td>0.40 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 2</td>
+<td>31241 - 41320</td>
+<td>2.50 ABET</td>
+<td>2.00 ABET</td>
+<td>0.50 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 3</td>
+<td>41321 - 51400</td>
+<td>3.00 ABET</td>
+<td>2.40 ABET</td>
+<td>0.60 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 4</td>
+<td>51401 - 61480</td>
+<td>3.50 ABET</td>
+<td>2.80 ABET</td>
+<td>0.70 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 5</td>
+<td>61481 - 71560</td>
+<td>4.00 ABET</td>
+<td>3.20 ABET</td>
+<td>0.80 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 6</td>
+<td>71561 - 81640</td>
+<td>4.50 ABET</td>
+<td>3.60 ABET</td>
+<td>0.90 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 7</td>
+<td>81641 - 91720</td>
+<td>5.00 ABET</td>
+<td>4.00 ABET</td>
+<td>1.00 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 8</td>
+<td>91721 - 101800</td>
+<td>5.50 ABET</td>
+<td>4.40 ABET</td>
+<td>1.10 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 9</td>
+<td>101801 - 111880</td>
+<td>6.00 ABET</td>
+<td>4.80 ABET</td>
+<td>1.20 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 10</td>
+<td>111881 - 121960</td>
+<td>6.50 ABET</td>
+<td>5.20 ABET</td>
+<td>1.30 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 11</td>
+<td>121961 - 132040</td>
+<td>7.00 ABET</td>
+<td>5.60 ABET</td>
+<td>1.40 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 12</td>
+<td>132041 - 142120</td>
+<td>7.50 ABET</td>
+<td>6.00 ABET</td>
+<td>1.50 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 13</td>
+<td>142121 - 152200</td>
+<td>8.00 ABET</td>
+<td>6.40 ABET</td>
+<td>1.60 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 14</td>
+<td>152201 - 162280</td>
+<td>8.50 ABET</td>
+<td>6.80 ABET</td>
+<td>1.70 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 15</td>
+<td>162281 - 172360</td>
+<td>9.00 ABET</td>
+<td>7.20 ABET</td>
+<td>1.80 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 16</td>
+<td>172361 - 182440</td>
+<td>9.50 ABET</td>
+<td>7.60 ABET</td>
+<td>1.90 ABET</td>
+<td>1 WEEK</td>
+</tr><tr>
+<td>PHASE 17</td>
+<td>182441 - 2167282</td>
+<td>10.00 ABET</td>
+<td>8.00 ABET</td>
+<td>2.00 ABET</td>
+<td>4 YEARS</td>
+</tr></tbody></table>
+<a href="https://altbet.io" title="altbet website" target="_blank">View our website for more details</a>
---
+++ src/chainparams.cpp
@@ -0,0 +1,374 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Copyright (c) 2014-2015 The Dash developers
+// Copyright (c) 2015-2017 The PIVX developers
+// Copyright (c) 2018 altbet.io
+//  
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "chainparams.h"
+
+#include "random.h"
+#include "util.h"
+#include "utilstrencodings.h"
+
+#include <assert.h>
+
+#include <boost/assign/list_of.hpp>
+
+using namespace std;
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+
+/**
+ * Main network
+ */
+
+//! Convert the pnSeeds6 array into usable address objects.
+static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data, unsigned int count)
+{
+    // It'll only connect to one or two seed nodes because once it connects,
+    // it'll get a pile of addresses with newer timestamps.
+    // Seed nodes are given a random 'last seen time' of between one and two
+    // weeks ago.
+    const int64_t nOneWeek = 7 * 24 * 60 * 60;
+    for (unsigned int i = 0; i < count; i++) {
+        struct in6_addr ip;
+        memcpy(&ip, data[i].addr, sizeof(ip));
+        CAddress addr(CService(ip, data[i].port));
+        addr.nTime = GetTime() - GetRand(nOneWeek) - nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+//   What makes a good checkpoint block?
+// + Is surrounded by blocks with reasonable timestamps
+//   (no blocks before with a timestamp after, none after with
+//    timestamp before)
+// + Contains no strange transactions
+static Checkpoints::MapCheckpoints mapCheckpoints =
+    boost::assign::map_list_of
+    (0, uint256("0x0000005088ab9f67a8f3edbf670f0df1fa5702aea8eafc78307667534472c37d"));
+static const Checkpoints::CCheckpointData data = {
+    &mapCheckpoints,
+    1539605437, // * UNIX timestamp of last checkpoint block
+    0,          // * total number of transactions between genesis and last checkpoint
+                //   (the tx=... number in the SetBestChain debug.log lines)
+    2000        // * estimated number of transactions per day after checkpoint
+};
+
+static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
+    boost::assign::map_list_of(0, uint256("0x001"));
+static const Checkpoints::CCheckpointData dataTestnet = {
+    &mapCheckpointsTestnet,
+    1524873600,
+    0,
+    250};
+
+static Checkpoints::MapCheckpoints mapCheckpointsRegtest =
+    boost::assign::map_list_of(0, uint256("0x001"));
+static const Checkpoints::CCheckpointData dataRegtest = {
+    &mapCheckpointsRegtest,
+    1524873600,
+    0,
+    100};
+
+class CMainParams : public CChainParams
+{
+public:
+    CMainParams()
+    {
+        networkID = CBaseChainParams::MAIN;
+        strNetworkID = "main";
+	    pchMessageStart[0] = 0x2a; // A
+        pchMessageStart[1] = 0x2b; // B
+        pchMessageStart[2] = 0x3e; // E
+        pchMessageStart[3] = 0x88; // T-8
+        vAlertPubKey = ParseHex("0435401a5693de702378a538ad939ea03b5e3b2826f68431ba840df5f418e3f3e8590ba4fb0c36097179bb55ffc4a4e065dc2ff87edb6f3ce4232a71d253ed7fff");
+        nDefaultPort = 2238;
+        bnProofOfWorkLimit = ~uint256(0) >> 20; // Altbet starting difficulty is 1 / 2^12
+        nSubsidyHalvingInterval = 210000;
+        nMaxReorganizationDepth = 100;
+        nEnforceBlockUpgradeMajority = 750;
+        nRejectBlockOutdatedMajority = 950;
+        nToCheckBlockUpgradeMajority = 1000;
+        nMinerThreads = 0;
+        nTargetTimespan = 1 * 60; // Altbet: 1 day
+        nTargetSpacing = 1 * 60;  // Altbet: 1 minute
+        nLastPOWBlock = 200;
+        nMaturity = 15;
+        nMasternodeCountDrift = 20;
+        nMasternodeCollateralLimit = 1000;
+        nModifierUpdateBlock = 615800;
+        nMaxMoneyOut = 21000000 * COIN;
+        const char* pszTimestamp = "Winner, winner chicken dinner.";
+        CMutableTransaction txNew;
+        txNew.vin.resize(1);
+        txNew.vout.resize(1);
+        txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        txNew.vout[0].nValue = 1 * COIN;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("04e219a879b053d19f67f06f8f0b409d6dd3ce854db1c384c68990474ff1da4e6b2f55dd5ebeb176a8f271a11d898ee5f129066481b0ae8eeaab8d6c4a61d6e2fe") << OP_CHECKSIG;
+        genesis.vtx.push_back(txNew);
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime = 1539605437;
+        genesis.nBits = 0x1e0ffff0;
+        genesis.nNonce = 2918613;
+
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x0000005088ab9f67a8f3edbf670f0df1fa5702aea8eafc78307667534472c37d"));
+        assert(genesis.hashMerkleRoot == uint256("0x73b17d2797b7b4d0a6fe09204aa736b6c1d290d3a6a6f944b7eab02f7029d0af"));
+
+        vSeeds.push_back(CDNSSeedData("clubs.seed.altbet.io", "clubs.seed.altbet.io"));
+        vSeeds.push_back(CDNSSeedData("hearts.seed.altbet.io", "hearts.seed.altbet.io"));
+        vSeeds.push_back(CDNSSeedData("diamonds.seed.altbet.io", "diamonds.seed.altbet.io"));
+        vSeeds.push_back(CDNSSeedData("spades.seed.altbet.io", "spades.seed.altbet.io"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 23); // A
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 25); // B
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 33); // E
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x02)(0x2D)(0x25)(0x33).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x02)(0x21)(0x31)(0x2B).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x77).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
+
+        fRequireRPCPassword = true;
+        fMiningRequiresPeers = false;		// default true
+        fAllowMinDifficultyBlocks = false;
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = true;
+        fMineBlocksOnDemand = false;
+        fSkipProofOfWorkCheck = true;		// default false
+        fTestnetToBeDeprecatedFieldRPC = false;
+        fHeadersFirstSyncingActive = false;
+
+        nPoolMaxTransactions = 3;
+        strSporkKey = "0469C3EA8B850A960FE799FAD34B34ED88037FCCBB9DA1B03472B0B3A23FDF16B0D9DC67AF8414423FC420286CE4319993976A6DCA09544BD739CAB6FDF777C662";
+        strObfuscationPoolDummyAddress = "Ab5bNTKMKVJWLTDCwwEEvHH9MzDhxRaL5a";
+        nStartMasternodePayments = 1539605437;
+
+    }
+
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return data;
+    }
+};
+static CMainParams mainParams;
+
+/**
+ * Testnet (v3)
+ */
+class CTestNetParams : public CMainParams
+{
+public:
+    CTestNetParams()
+    {
+        networkID = CBaseChainParams::TESTNET;
+        strNetworkID = "test";
+        pchMessageStart[0] = 0x41;
+        pchMessageStart[1] = 0x4d;
+        pchMessageStart[2] = 0x5e;
+        pchMessageStart[3] = 0x78;
+        vAlertPubKey = ParseHex("043e8760d1c9ef3af5a5e49796afe4389a5cb53c6028b54b9af0a152f34762e453615a1aab9260a31045b85f87d4de36bbe6fd04478fcc103fd47c8e1b813c3d3c");
+        nDefaultPort = 39795;
+        nEnforceBlockUpgradeMajority = 51;
+        nRejectBlockOutdatedMajority = 75;
+        nToCheckBlockUpgradeMajority = 100;
+        nMinerThreads = 0;
+        nTargetTimespan = 1 * 60; // Altbet: 1 day
+        nTargetSpacing = 1 * 60;  // Altbet: 1 minute
+        nLastPOWBlock = 200;
+        nMaturity = 15;
+        nMasternodeCountDrift = 4;
+        nMasternodeCollateralLimit = 1000;
+        nModifierUpdateBlock = 51197; //approx Mon, 17 Apr 2017 04:00:00 GMT
+        nMaxMoneyOut = 43199500 * COIN;
+
+        //! Modify the testnet genesis block so the timestamp is valid for a later start.
+        genesis.nTime = 1524873600;
+        genesis.nNonce = 2394236;
+
+        hashGenesisBlock = genesis.GetHash();
+        // assert(hashGenesisBlock == uint256("00000dfdec6a9190b26520e93ef5eba15a82646a47ddf8fb5be4477a836467ee"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 139); // Testnet altbet addresses start with 'x' or 'y'
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 19);  // Testnet altbet script addresses start with '8' or '9'
+        base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 239);     // Testnet private keys start with '9' or 'c' (Bitcoin defaults)
+        // Testnet altbet BIP32 pubkeys start with 'DRKV'
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x3a)(0x80)(0x61)(0xa0).convert_to_container<std::vector<unsigned char> >();
+        // Testnet altbet BIP32 prvkeys start with 'DRKP'
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x3a)(0x80)(0x58)(0x37).convert_to_container<std::vector<unsigned char> >();
+        // Testnet altbet BIP44 coin type is '1' (All coin's testnet default)
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0x01).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
+
+        fRequireRPCPassword = true;
+        fMiningRequiresPeers = false;
+        fAllowMinDifficultyBlocks = true;
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = false;
+        fSkipProofOfWorkCheck = true;
+        fTestnetToBeDeprecatedFieldRPC = true;
+
+        nPoolMaxTransactions = 2;
+        strSporkKey = "04cded1204a57acd6280c8499b7a2df052609dbf96546453984d632204d651d72a37013edc9d115e5a385e100eb7e867923fdd0bb7d9dc31aa1eb9d59b00c76697";
+        strObfuscationPoolDummyAddress = "xxVKdbxVogrXrPLMo2qEEyCm1GRv2KZCLy";
+        nStartMasternodePayments = 1524873600; //Fri, 09 Jan 2015 21:05:58 GMT
+    }
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return dataTestnet;
+    }
+};
+static CTestNetParams testNetParams;
+
+/**
+ * Regression test
+ */
+class CRegTestParams : public CTestNetParams
+{
+public:
+    CRegTestParams()
+    {
+        networkID = CBaseChainParams::REGTEST;
+        strNetworkID = "regtest";
+        strNetworkID = "regtest";
+        pchMessageStart[0] = 0x2d;
+        pchMessageStart[1] = 0x53;
+        pchMessageStart[2] = 0x6f;
+        pchMessageStart[3] = 0x40;
+        nSubsidyHalvingInterval = 150;
+        nEnforceBlockUpgradeMajority = 750;
+        nRejectBlockOutdatedMajority = 950;
+        nToCheckBlockUpgradeMajority = 1000;
+        nMinerThreads = 1;
+        nTargetTimespan = 24 * 60 * 60; // Altbet: 1 day
+        nTargetSpacing = 1 * 60;        // Altbet: 1 minutes
+        bnProofOfWorkLimit = ~uint256(0) >> 1;
+        genesis.nTime = 1524873600;
+        genesis.nBits = 0x207fffff;
+        genesis.nNonce = 906460;
+
+        hashGenesisBlock = genesis.GetHash();
+        nDefaultPort = 39793;
+        // assert(hashGenesisBlock == uint256("00000d885e2813770fd59e71010b6b62a9b0609655109bf4e1b24c3bd524ae0c"));
+
+        vFixedSeeds.clear(); //! Testnet mode doesn't have any fixed seeds.
+        vSeeds.clear();      //! Testnet mode doesn't have any DNS seeds.
+
+        fRequireRPCPassword = false;
+        fMiningRequiresPeers = false;
+        fAllowMinDifficultyBlocks = true;
+        fDefaultConsistencyChecks = true;
+        fRequireStandard = false;
+        fMineBlocksOnDemand = true;
+        fTestnetToBeDeprecatedFieldRPC = false;
+    }
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        return dataRegtest;
+    }
+};
+static CRegTestParams regTestParams;
+
+/**
+ * Unit test
+ */
+class CUnitTestParams : public CMainParams, public CModifiableParams
+{
+public:
+    CUnitTestParams()
+    {
+        networkID = CBaseChainParams::UNITTEST;
+        strNetworkID = "unittest";
+        nDefaultPort = 39791;
+        vFixedSeeds.clear(); //! Unit test mode doesn't have any fixed seeds.
+        vSeeds.clear();      //! Unit test mode doesn't have any DNS seeds.
+
+        fRequireRPCPassword = false;
+        fMiningRequiresPeers = false;
+        fDefaultConsistencyChecks = true;
+        fAllowMinDifficultyBlocks = false;
+        fMineBlocksOnDemand = true;
+    }
+
+    const Checkpoints::CCheckpointData& Checkpoints() const
+    {
+        // UnitTest share the same checkpoints as MAIN
+        return data;
+    }
+
+    //! Published setters to allow changing values in unit test cases
+    virtual void setSubsidyHalvingInterval(int anSubsidyHalvingInterval) { nSubsidyHalvingInterval = anSubsidyHalvingInterval; }
+    virtual void setEnforceBlockUpgradeMajority(int anEnforceBlockUpgradeMajority) { nEnforceBlockUpgradeMajority = anEnforceBlockUpgradeMajority; }
+    virtual void setRejectBlockOutdatedMajority(int anRejectBlockOutdatedMajority) { nRejectBlockOutdatedMajority = anRejectBlockOutdatedMajority; }
+    virtual void setToCheckBlockUpgradeMajority(int anToCheckBlockUpgradeMajority) { nToCheckBlockUpgradeMajority = anToCheckBlockUpgradeMajority; }
+    virtual void setDefaultConsistencyChecks(bool afDefaultConsistencyChecks) { fDefaultConsistencyChecks = afDefaultConsistencyChecks; }
+    virtual void setAllowMinDifficultyBlocks(bool afAllowMinDifficultyBlocks) { fAllowMinDifficultyBlocks = afAllowMinDifficultyBlocks; }
+    virtual void setSkipProofOfWorkCheck(bool afSkipProofOfWorkCheck) { fSkipProofOfWorkCheck = afSkipProofOfWorkCheck; }
+};
+static CUnitTestParams unitTestParams;
+
+
+static CChainParams* pCurrentParams = 0;
+
+CModifiableParams* ModifiableParams()
+{
+    assert(pCurrentParams);
+    assert(pCurrentParams == &unitTestParams);
+    return (CModifiableParams*)&unitTestParams;
+}
+
+const CChainParams& Params()
+{
+    assert(pCurrentParams);
+    return *pCurrentParams;
+}
+
+CChainParams& Params(CBaseChainParams::Network network)
+{
+    switch (network) {
+    case CBaseChainParams::MAIN:
+        return mainParams;
+    case CBaseChainParams::TESTNET:
+        return testNetParams;
+    case CBaseChainParams::REGTEST:
+        return regTestParams;
+    case CBaseChainParams::UNITTEST:
+        return unitTestParams;
+    default:
+        assert(false && "Unimplemented network");
+        return mainParams;
+    }
+}
+
+void SelectParams(CBaseChainParams::Network network)
+{
+    SelectBaseParams(network);
+    pCurrentParams = &Params(network);
+}
+
+bool SelectParamsFromCommandLine()
+{
+    CBaseChainParams::Network network = NetworkIdFromCommandLine();
+    if (network == CBaseChainParams::MAX_NETWORK_TYPES)
+        return false;
+
+    SelectParams(network);
+    return true;
+}
