3ff16ae8938324ad5d13d850609b509173df760f
MotoAcidic <travisfinch01@gmail.com>
2019-01-26 00:30:35 -0500

    Install Scripts

---
+++ scripts/build-maintar.sh
@@ -0,0 +1,4 @@
+cd ..
+cd src
+strip altbet{d,-cli}
+tar cvzf ALTBET-linux.tar.gz altbetd altbet-cli
--- scripts/buildwindows32.sh
+++ scripts/buildwindows32.sh
@@ -1,3 +1,4 @@
+cd ..
 sudo apt update
 sudo apt upgrade
 sudo apt install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git
@@ -12,6 +13,12 @@ sudo apt-get install libzmq3-dev -y
 sudo apt-get install libminiupnpc-dev -y
 sudo apt-get install libgmp3-dev libevent-dev bsdmainutils libboost-all-dev openssl -y
 
-./autogen.sh
-./configure
-make
+sudo apt install g++-mingw-w64-i686 mingw-w64-i686-dev
+sudo update-alternatives --config i686-w64-mingw32-g++
+PATH=$(echo "$PATH" | sed -e 's/:\/mnt.*//g') # strip out problematic Windows %PATH% imported var
+cd depends
+make HOST=i686-w64-mingw32
+
+cd ..
+cd scripts
+bash build-win32.sh
